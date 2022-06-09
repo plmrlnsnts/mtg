8e387674c3280a0b4f4714834789d5b30e677b02
Paul Santos <paulmarlonsantos@gmail.com>
2022-06-09 20:36:30 +0800

    📦 install composer deps

---
+++ config/cloudinary.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'cloud_url' => env('CLOUDINARY_URL'),
+    'notification_url' => env('CLOUDINARY_NOTIFICATION_URL'),
+    'upload_preset' => env('CLOUDINARY_UPLOAD_PRESET'),
+];
